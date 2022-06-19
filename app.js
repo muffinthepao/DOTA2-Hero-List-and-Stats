e511e669fbd1cab5b9b63d6a6b5e8f2fee68558b
Mervin <Mervin@Mervins-MBP.lan>
2022-06-19 21:28:23 +0800

    trying to toggle filter

--- app.js
+++ app.js
@@ -3,6 +3,12 @@
 const url = "https://api.opendota.com/api/heroStats";
 const imgUrl = "https://api.opendota.com";
 
+const strAttributeImg = "./attribute-images/strength.png";
+const agiAttributeImg = "./attribute-images/agility.png";
+const intAttributeImg = "./attribute-images/intelligence.png";
+
+const filteredBy = document.querySelector("#filter-bar");
+
 //get list of heroes from API
 const data = async function getListOfHeroes(url) {
     try {
@@ -55,34 +61,105 @@ class Hero {
 }
 
 class Game {
-    arrayOfHeroes = [];
+    //list of heroes [Hero, Hero, Hero, Hero]
     constructor(heroes) {
-        //list of heroes [Hero, Hero, Hero, Hero]
-        this.arrayOfHeroes = heroes;
+
+        //source of truth for all heroes
+        this.arrayOfHeroes = heroes
+        //
+        this.heroesToDisplay = heroes
         this.display();
+        // this.checker = this.checkIfThereIsOnlyOneAttribute();
     }
+
+    //sort array by hero name alphabetically
 
     //display images on index.html
     display() {
-        return this.arrayOfHeroes.forEach((element) => {
+        console.log('displaying')
+        // console.log(this.heroesToDisplay)
+        this.heroesToDisplay.forEach((element) => {
+            console.log('+')
+
+            function whichArrtibutePic() {
+                if (element.primaryAttri === "str") {
+                    return strAttributeImg;
+                }
+
+                if (element.primaryAttri === "agi") {
+                    return agiAttributeImg;
+                }
+
+                if (element.primaryAttri === "int") {
+                    return intAttributeImg;
+                }
+            }
+            
             const push = document.querySelector("#hero-list");
             const htmlElements = `
-                <div class="hero-image">
-                    <img src="${imgUrl}${element.image}"/>
+                <div class="hero-image" style="background-image: url(&quot;${imgUrl}${element.image}&quot)">
+                    <div class="hero-name">
+                        <img src="${whichArrtibutePic()}"/>
+                        <h4>${element.heroName}</h4>
+                    </div>
                 </div>
-                `;
+            `;
             push.insertAdjacentHTML("beforeend", htmlElements);
         });
     }
+
+    filterByAttribute(id, element) {
+        // console.log("parent: ", event.target.parentElement);
+        // console.log("element: ", event.target);
+
+        const push = document.querySelector("#hero-list");
+        push.innerHTML = ''            
+
+        if (id === "strFilter" && element.classList !== "active") {
+            console.log("clicked on strength")
+            this.heroesToDisplay = this.arrayOfHeroes.filter(x => x.primaryAttri === "str")
+            this.display()
+            element.classList.toggle("active")
+
+        }
+
+        if (id === "agiFilter" && element.classList !== "active") {
+            console.log("clicked on agility")
+            this.heroesToDisplay = this.arrayOfHeroes.filter(x => x.primaryAttri === "agi")
+            this.display()
+            element.classList.toggle("active")
+        }
+
+        if (id === "intFilter" && element.classList !== "active") {
+            console.log("clicked on intelligence");
+            this.heroesToDisplay = this.arrayOfHeroes.filter(x => x.primaryAttri === "int")
+            this.display()
+            element.classList.toggle("active")
+        }
+
+        return this.display();
+    
+        // };
+       // return heroes
+   }
+
 }
 
 async function init() {
     // function create instances of hero(s)
-    const heroes = await data(url);
+    let heroes = await data(url);
+    // let filteredHeroes = await strHeroes(url)
 
-    //array of hero instances push into new Dota2 class
+    // //array of hero instances push into new Dota2 class
     const Dota2 = new Game(heroes);
     console.log("List of heroes: ", Dota2);
+    // let dota3 = Dota2.arrayOfHeroes.filter(x=>x.primaryAttri==="str")
+    // console.log(dota3)
+
+    filteredBy.onclick = function (event) {
+        Dota2.filterByAttribute(event.target.id, event.target)
+    }
+
 }
 
 init();
---
+++ scratchpad.js
@@ -0,0 +1,21 @@
+// let picks = [];
+            // let wins = [];
+            // Object.keys(hero).forEach((key) => {
+            //     if (key.indexOf("_pick") > -1) {
+            //         picks.push(key);
+            //     } else if (key.indexOf("_win") > -1) {
+            //         wins.push(key);
+            //     }
+            // });
+
+            // picks = picks.map((key) => hero[key]);
+            // wins = wins.map((key) => hero[key]);
+
+            // const picks = [
+            //     hero['1_pick'],
+            //     hero['2_pick'],
+            //     hero['3_pick'],
+            //     hero['4_pick'],
+            //     hero['5_pick'],
+            //     hero['6_pick'],
+            // ]
